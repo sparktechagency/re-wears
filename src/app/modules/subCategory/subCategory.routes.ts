15e402fc3e657cd0f43ddf5378f4a7a6cdafc648
iamrazzakk <rakibt23p@gmail.com>
2025-06-04 11:30:37 +0600

    chore: comment out authentication middleware in category, childSubCategory, and subCategory routes

--- src/app/modules/subCategory/subCategory.routes.ts
+++ src/app/modules/subCategory/subCategory.routes.ts
@@ -25,19 +25,19 @@ router.post(
       res.status(500).json({ message: "Failed to upload Image" });
     }
   },
-  auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN),
+  // auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN),
   SubCategoryController.createSubCategory
 );
 
 router.get(
   "/",
-  auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN, USER_ROLES.USER),
+  // auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN, USER_ROLES.USER),
   SubCategoryController.getAllSubCategory
 );
 
 router.get(
   "/:id",
-  auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN, USER_ROLES.USER),
+  // auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN, USER_ROLES.USER),
   SubCategoryController.getSingleSubCategory
 );
 
@@ -61,12 +61,12 @@ router.patch(
       res.status(500).json({ message: "Failed to upload Image" });
     }
   },
-  auth(USER_ROLES.SUPER_ADMIN),
+  // auth(USER_ROLES.SUPER_ADMIN),
   SubCategoryController.updateSubCategory
 );
 router.delete(
   "/:id",
-  auth(USER_ROLES.SUPER_ADMIN),
+  // auth(USER_ROLES.SUPER_ADMIN),
   SubCategoryController.deleteSubCategory
 );
 
