07355108362c5a8273e016422ff2e3d45d546712
iamrazzakk <rakibt23p@gmail.com>
2025-06-14 21:15:45 +0600

    feat: add authentication middleware to CMS routes for enhanced security

--- src/app/modules/cms/cms.route.ts
+++ src/app/modules/cms/cms.route.ts
@@ -2,15 +2,18 @@ import express from "express";
 import { CmsController } from "./cms.controller";
 import validateRequest from "../../middlewares/validateRequest";
 import { CmsValidations } from "./cms.validation";
+import auth from "../../middlewares/auth";
+import { USER_ROLES } from "../../../enums/user";
 
 const router = express.Router();
 
 router.put(
   "/",
+  auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN),
   validateRequest(CmsValidations.cmsSchema),
   CmsController.createOrUpdateCms
 );
-
-router.get("/:type", CmsController.getCms);
+// get all cms
+router.get("/:type", auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), CmsController.getCms);
 
 export const CmsRoutes = router;
