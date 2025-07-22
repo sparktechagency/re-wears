b0ba17604cd1ab52abc3f77ec4385f1617f3c605
iamrazzakk <rakibt23p@gmail.com>
2025-07-22 17:55:38 +0600

    console what token i get

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -6,44 +6,45 @@ import { AuthService } from './auth.service';
 import config from '../../../config';
 
 const verifyEmail = catchAsync(async (req: Request, res: Response) => {
-    const { ...verifyData } = req.body;
-    const result = await AuthService.verifyEmailToDB(verifyData);
+  const { ...verifyData } = req.body;
+  const result = await AuthService.verifyEmailToDB(verifyData);
 
-    sendResponse(res, {
-        success: true,
-        statusCode: StatusCodes.OK,
-        message: result.message,
-        data: result.data,
-    });
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: result.message,
+    data: result.data,
+  });
 });
 
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
-    const { ...loginData } = req.body;
-    const result = await AuthService.loginUserFromDB(loginData);
+  const { ...loginData } = req.body;
+  const result = await AuthService.loginUserFromDB(loginData);
 
-    sendResponse(res, {
-        success: true,
-        statusCode: StatusCodes.OK,
-        message: 'User login successfully',
-        data: result
-    });
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'User login successfully',
+    data: result
+  });
 });
 
 const forgetPassword = catchAsync(async (req: Request, res: Response) => {
-    const email = req.body.email;
-    const result = await AuthService.forgetPasswordToDB(email);
+  const email = req.body.email;
+  const result = await AuthService.forgetPasswordToDB(email);
 
-    sendResponse(res, {
-        success: true,
-        statusCode: StatusCodes.OK,
-        message: 'Please check your email, we send a OTP!',
-        data: result
-    });
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: 'Please check your email, we send a OTP!',
+    data: result
+  });
 });
 
 const resetPassword = catchAsync(async (req: Request, res: Response) => {
   const token = req.headers.authorization;
+  console.log(token);
   const { ...resetData } = req.body;
   const result = await AuthService.resetPasswordToDB(token!, resetData);
 
@@ -132,14 +133,14 @@ const deleteUserByEmailAndPassword = catchAsync(async (req: Request, res: Respon
 
 
 export const AuthController = {
-    verifyEmail,
-    loginUser,
-    forgetPassword,
-    resetPassword,
-    changePassword,
-    newAccessToken,
-    resendVerificationEmail,
-    socialLogin,
+  verifyEmail,
+  loginUser,
+  forgetPassword,
+  resetPassword,
+  changePassword,
+  newAccessToken,
+  resendVerificationEmail,
+  socialLogin,
   deleteUser,
   deleteUserByEmailAndPassword
 };
